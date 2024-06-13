96d6d32d8ec6f0f39982a3a51f86b3f26139e229
kchristmas <kirstinchristmas@gmail.com>
2024-06-13 15:56:25 -0400

    on author's task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,15 +18,38 @@ public_users.get('/books',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let reference = parseInt(req.params.isbn)
+  res.send(books[reference]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let findAuthor = req.params.author.toLowerCase();
+  let foundBook = []
+  for (let i =0; i < books.length; i++){
+    let bookOption = books[i].author.toLowerCase().replace(/\s/g, '');
+    if (bookOption === findAuthor){
+        foundBook.push(books[i])
+    }
+}
+   if (foundBook.length > 0){
+    res.send(foundBook)
+   } else {
+    res.send("Cannot find any books by that author")
+  }
 });
+
+//b1. Please note that you can get the correct output using 'Object.keys' as well.  It is just that you need to modify your code accordingly. 
+
+//b2. One such approach is:
+
+///1.  Initialize an empty array
+
+//2. use 'Object.keys()' to retrieve the keys (ie. book ISBNs)
+
+//3. Find the 'author' in the 'books' object matching the 'author' name provided in the HTTP request params using the forEach() iteration method.
+
+//4. Use the push() method to add the details of the book whose 'author' name matches the one given in the request params, and return the array as the output.
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
