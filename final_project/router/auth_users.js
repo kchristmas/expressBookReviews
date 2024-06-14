a6347e9becea12a95a18aa0f7b9957150d745bc5
kchristmas <kirstinchristmas@gmail.com>
2024-06-14 13:02:23 -0400

    finished task 6

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,9 +5,15 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username)=> {
+    let userswithsamename = users.filter((user)=>{
+        return user.username === username
+      });
+      if(userswithsamename.length > 0){
+        return true;
+      } else {
+        return false;
+}}
 
 const authenticatedUser = (username,password)=>{ 
     let validusers = users.filter((user)=>{
